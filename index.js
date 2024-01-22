f0b0bee1ecee78fd80d3e7762f95fad4d9a14b7c
Suryanarayan paul <110797692+suryaNpaul@users.noreply.github.com>
2024-01-22 18:55:59 +0530

    init commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function randomDice() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-  var randomImage1 = "/images/dice" + randomNumber1 + ".png";
+  var randomImage1 = "images/dice" + randomNumber1 + ".png";
 
   document.querySelectorAll("img")[0].setAttribute("src", randomImage1);
 
   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-  var randomImage2 = "/images/dice" + randomNumber2 + ".png";
+  var randomImage2 = "images/dice" + randomNumber2 + ".png";
   document.querySelectorAll("img")[1].setAttribute("src", randomImage2);
 
   if (randomNumber1 > randomNumber2) {
